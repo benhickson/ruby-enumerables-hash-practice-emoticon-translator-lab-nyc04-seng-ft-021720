2a9e1fa01281575a894fba3c37ef939217881d42
Ben Hickson <bzhickson@gmail.com>
2020-02-12 04:46:26 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -12,8 +12,8 @@ def load_library(path)
   output
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon(yaml, english_emoticon)
+  load_library(yaml)[:get_emoticon][english_emoticon]
 end
 
 def get_english_meaning
